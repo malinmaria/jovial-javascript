06cb38a6280da90eb0a85df2ca6127063f2beca4
Malin Heineking <mmheineking@gmail.com>
2015-11-15 23:20:24 -0500

    added comments

--- MatchingGame/MatchingGame.js
+++ MatchingGame/MatchingGame.js
@@ -6,36 +6,34 @@ function generateFaces(){
     
 	for(var i = 0; i < numberOfFaces; i++){
         
-		var face = document.createElement("img");
+		var face = document.createElement("img"); //creating img tag & set position in css
 		face.src = "http://home.cse.ust.hk/~rossiter/mooc/matching_game/smile.png";
         
-		var x = String(Math.floor(Math.random()*400));
+		var x = String(Math.floor(Math.random()*400)); //chooses random px location
 		var y = String(Math.floor(Math.random()*400));
 		var topStyle = "top:" + x + "px";
 		var leftStyle = "left:" + y + "px";
-        face.setAttribute("style",topStyle + ";" + leftStyle);
+        face.setAttribute("style",topStyle + ";" + leftStyle); //style is actually set here
 		theLeftSide.appendChild(face.cloneNode(true));
-		theRightSide.appendChild(face);
-		<!-- alert("is this working?"); -->
-		
+		theRightSide.appendChild(face); //clone img to right side
 	};
         
 var removeFace = document.getElementById("leftSide").lastElementChild;
-theLeftSide.removeChild(removeFace);
+theLeftSide.removeChild(removeFace); //removing last smile from left side
 var theBody = document.getElementsByTagName("body")[0];
 
 
-theRightSide.lastChild.onclick = function nextLevel(event){
+theRightSide.lastChild.onclick = function nextLevel(event){ //event handler adding 5 more smiles if guessed correctly
 	event.stopPropagation();
 	numberOfFaces += 5;
-    while(theLeftSide.firstChild)
+    while(theLeftSide.firstChild) //replaces last missing child after each round
         theLeftSide.removeChild(theLeftSide.firstChild);
     while(theRightSide.firstChild)
         theRightSide.removeChild(theRightSide.firstChild);
 	generateFaces();
 };
 
-theBody.onclick = function gameOver(){
+theBody.onclick = function gameOver(){ //event handler ending game if guessed incorrectly
 	alert("Game over!");
 	theBody.onclick = null;
 	theRightSide.lastChild.onclick = null;
