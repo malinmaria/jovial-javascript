ec365794fc445d041ddcd096e6974b04007a7565
Malin Nemergut <mmheineking@gmail.com>
2016-04-03 23:34:39 -0400

    initial commit

---
+++ Clock/jquery.tzineClock.js
@@ -0,0 +1,13 @@
+<div class="green clock"></div>
+
+<div class="display"></div>
+
+<div class="front left"></div>
+
+<div class="rotate left">
+  <div class="bg left"></div>
+</div>
+
+<div class="rotate right">
+    <div class="bg right"></div>
+</div>    
