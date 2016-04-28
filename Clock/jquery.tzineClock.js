1398db9a8af9d32333a9d0c60df01138046e4624
malinmaria <mmheineking@gmail.com>
2016-04-28 13:14:15 -0400

    added logic to setup function

--- Clock/jquery.tzineClock.js
+++ Clock/jquery.tzineClock.js
@@ -5,6 +5,19 @@ function($){
 
 	// Extending the jQuery core:
 	$.fn.tzineClock = function(opts){
+      //if the selector returned more than one element, we use the first one
+      var container = this.eq(0);
+      if(!container){
+        try{
+          console.log("Invalid selector");
+        } catch(e){}
+        return false;
+      }
+      if(!opts) opts = {};
+
+      setup.call(container);
+
+      return this;
 }
 
 function setup(){
@@ -12,7 +25,7 @@ function setup(){
   var colors = [orange,blue,green];
 
   var tmp;
-  for(var = i; i < 3; i++){
+  for(var = i=0; i < 3; i++){
     tmp = $('<div>').attr('class',colors[i]+' clock').html(
       '<div class="green clock"></div>'+
       '<div class="display"></div>'+
@@ -24,6 +37,17 @@ function setup(){
       '<div class="bg right"></div>'+
       '</div>'
     );
+
+    //Appending to the fancyClock container
+    $(this).append(tmp);
+
+    //assigning some elements as variables for speed
+    tmp.rotateLeft = tmp.find('.rotate.left');
+    tmp.rotateRight = tmp.find('.rotate.right');
+    tmp.display = tmp.find('.display');
+
+    //adding the dial as a global variable, available as gVars.colorName
+    gVars[colors[i]] = tmp;
   };
 
     setInterval(function(){
