bef7c975d17d79ef5d5d33b3dbbcecd45c05248d
Malin Nemergut <mmheineking@gmail.com>
2017-01-03 18:57:34 -0500

    adding same JS functionality for minute and hour hands

--- DailyJavaScriptExercises/Day1-Clock/js/scripts.js
+++ DailyJavaScriptExercises/Day1-Clock/js/scripts.js
@@ -1,11 +1,21 @@
 const secondHand = document.querySelector('.second');
+const minuteHand = document.querySelector('.minute');
+const hourHand = document.querySelector('.hour');
 
 function setDate() {
   const now = new Date();
+
   const seconds = now.getSeconds();
   const secondsDeg = ((seconds/60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDeg}deg)`;
-  console.log(seconds);
+
+  const mins = now.getMinutes();
+  const minsDeg = ((mins/60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minsDeg}deg)`;
+
+  const hour = now.getMinutes();
+  const hourDeg = ((hour/12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
 }
 
 setInterval(setDate, 1000);
