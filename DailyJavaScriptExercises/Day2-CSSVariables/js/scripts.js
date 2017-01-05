6a58dda0c80187791a6e2755f116a59fbbb2f0b2
Malin Nemergut <mmheineking@gmail.com>
2017-01-04 22:12:05 -0500

    using dataset to define suffix, then using setProperty to update input name

--- DailyJavaScriptExercises/Day2-CSSVariables/js/scripts.js
+++ DailyJavaScriptExercises/Day2-CSSVariables/js/scripts.js
@@ -1,8 +1,17 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate(){
-  console.log(this.value);
+  // dataset is an object that will contain all 'data-' attributes you created
+  //  from each specific element, don't have to use any selectors for it
+  // doing || '' because color doesn't have dataset and it will appened 'undefined' if this is not specified
+  const suffix = this.dataset.sizing || '';
+  //selecting css variable by first selecting entire document,
+  //then setting a property of each variable (color, spacing, blur)-notice we defined a name for each input
+  //this.name then gets set to this.value
+  //adding suffix variable to add px to each value
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
+
 
 //loops over each input, then when 'change' event is called then handleUpdate is called
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
