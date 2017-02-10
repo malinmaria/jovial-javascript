26e4d188cbe44c2af340b29fa639c79eeb0a33bb
Malin Nemergut <mmheineking@gmail.com>
2017-02-10 14:00:38 -0500

    setting variables and using event listeners

--- DailyJavaScriptExercises/Day7-Canvas/js/scripts.js
+++ DailyJavaScriptExercises/Day7-Canvas/js/scripts.js
@@ -12,3 +12,13 @@ ctx.lineCap = 'round';
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+
+function draw(e){
+  if(!isDrawing) return; //stops fn from running when not moused down
+  console.log(e);
+};
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
