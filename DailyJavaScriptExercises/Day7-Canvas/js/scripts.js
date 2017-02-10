96b36aace6414add55e6db50bd29b3d10a2ff925
Malin Nemergut <mmheineking@gmail.com>
2017-02-10 14:31:18 -0500

    styling line and using hsl to change its colors

--- DailyJavaScriptExercises/Day7-Canvas/js/scripts.js
+++ DailyJavaScriptExercises/Day7-Canvas/js/scripts.js
@@ -8,14 +8,17 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 50;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 function draw(e){
   if(!isDrawing) return; //stops fn from running when not moused down
   console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   //start from
   ctx.moveTo(lastX, lastY);
@@ -23,6 +26,7 @@ function draw(e){
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue++;
 };
 
 canvas.addEventListener('mousemove', draw);
