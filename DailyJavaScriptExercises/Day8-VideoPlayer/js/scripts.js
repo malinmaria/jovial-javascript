b941653c170b9a11e381da28fb43c480cfcdf7ba
Malin Nemergut <mmheineking@gmail.com>
2017-02-16 23:03:38 -0500

    defining js elements

---
+++ DailyJavaScriptExercises/Day8-VideoPlayer/js/scripts.js
@@ -0,0 +1,10 @@
+//Get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress_filled');
+const toggle = player.querySelector('.toggle');
+
+//Build Functions
+
+//Hook up event listeners
