780e2692ded623145fc1c30d55e17e0132d17433
Malin Nemergut <mmheineking@gmail.com>
2017-02-16 23:12:42 -0500

    adding toggle play/pause video function and event listeners

--- DailyJavaScriptExercises/Day8-VideoPlayer/js/scripts.js
+++ DailyJavaScriptExercises/Day8-VideoPlayer/js/scripts.js
@@ -6,5 +6,14 @@ const progressBar = player.querySelector('.progress_filled');
 const toggle = player.querySelector('.toggle');
 
 //Build Functions
+function togglePlay() {
+  if(video.paused){
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
 //Hook up event listeners
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
