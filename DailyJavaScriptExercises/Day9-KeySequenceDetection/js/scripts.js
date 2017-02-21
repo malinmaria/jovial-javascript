7567c6372892b58dc9de6c1e9ca8c90e0302e70e
Malin Nemergut <mmheineking@gmail.com>
2017-02-21 12:43:40 -0500

    checking key sequence and displays cornify fn if there is a match

--- DailyJavaScriptExercises/Day9-KeySequenceDetection/js/scripts.js
+++ DailyJavaScriptExercises/Day9-KeySequenceDetection/js/scripts.js
@@ -1,1 +1,16 @@
+const pressed = [];
 const code = 'malin';
+
+window.addEventListener('keyup', (e) => {
+  console.log(e.key);
+  pressed.push(e.key);
+  //starts from the end of 'code' and goes back 5 letters to make array exact length we need
+  pressed.splice(-code.length - 1, pressed.length - code.length);
+  //checks for 'code' we're looking for, .join('') turns what was typed into a string
+  if(pressed.join('').includes(code)){
+    // console.log("you got it!");
+    cornify_add();
+  }
+
+  console.log(pressed);
+})
