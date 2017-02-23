4d5e70f1f831d7dbab06869474434c8ac1fff0ea
Malin Nemergut <mmheineking@gmail.com>
2017-02-22 22:06:20 -0500

    built out checkSlide fn to add or remove img slide class based on scroll location

--- DailyJavaScriptExercises/Day10-SlideInOnScroll/js/scripts.js
+++ DailyJavaScriptExercises/Day10-SlideInOnScroll/js/scripts.js
@@ -2,7 +2,22 @@ const sliderImgs = document.querySelectorAll('.slide-in');
 
 //runs everytime user scrolls
 function checkSlide(e) {
-  console.count(e);
+  sliderImgs.forEach(sliderImg => {
+    //gives px location at the bottom of the window, then subtracts half of the img height
+    //this is done so we can start sliding the img in when half of it is shown
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImg.height / 2;
+    //same thing but for when the user is scrolling back up the page
+    //first finding px location of bottom of img, then finds half img location, then finds when img is scrolled past
+    const imgBottom = sliderImg.offsetTop + sliderImg.height;
+    const isHalfShown = slideInAt > sliderImg.offsetTop;
+    const notScrolledPast = window.scrollY < imgBottom;
+    //if both are true then active class is added, else it is removed
+    if(isHalfShown && notScrolledPast){
+      sliderImg.classList.add('active');
+    }else {
+      sliderImg.classList.remove('active');
+    }
+  });
 }
 
 //stops checkSlide function from running too many times
